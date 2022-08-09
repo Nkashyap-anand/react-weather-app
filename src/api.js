ad67edccfe784e6ad5ac0281a637a4886e6b671f
nkashyap <nkashyap.anand@gmail.com>
2022-08-09 13:56:08 +0530

    react weather app start

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd2563a7bbamsha318ffc1153c0b1p12f47fjsnd3059d5a35f7',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+	},
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = 'f9347d1882ec467868cac2fb44d199d9';
